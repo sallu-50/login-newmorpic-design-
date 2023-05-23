67e90f6b6b433e049cf2854321a1790a14530ee4
sallu-50 <salmanabdullahal0@gmail.com>
2023-05-23 14:07:33 +0600

    Add files via upload

---
+++ ppp.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Form Validation Example</title>
+  <script src="script.js" defer></script>
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>registration page</title>
+    <link rel="stylesheet" href="fonts/css/font-awesome.min.css" >
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" >
+    <link rel="stylesheet" href="style.css"> <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Toggle Password Visibility</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/stylee.css" />
+
+    <style>
+        body{
+            background-color: #ecf0f3;
+        }
+        .login-div {
+            margin: auto;
+            width:400px;
+            height: 650px;
+            padding: 60px 35px 35px 35px;
+            border-radius: 40px;
+            background: #ecf0f3;
+            box-shadow: 13px 13px 20px #cbced1,
+                        -13px -13px 20px #ffffff;
+}
+.logo {
+  background:url("Logo.png");
+  width:100px;
+  height: 100px;
+  border-radius: 50%;
+  margin:0 auto;
+  box-shadow: 
+  /* logo shadow */
+  0px 0px 2px #5f5f5f,
+  /* offset */
+  0px 0px 0px 5px #ecf0f3,
+  /*bottom right */
+  8px 8px 15px #a7aaaf,
+  /* top left */
+  -8px -8px 15px #ffffff
+  ;
+}
+.title {
+  text-align: center;
+  font-size: 28px;
+  padding-top: 24px;
+  letter-spacing: 0.5px;
+}
+.sub-title {
+  text-align: center;
+  font-size: 15px;
+  padding-top: 7px;
+  letter-spacing: 3px;
+}
+.fields {
+  width: 100%;
+  padding: 75px 5px 5px 5px;
+}
+.fields input {
+  border: none;
+  outline:none;
+  background: none;
+  font-size: 18px;
+  color: #555;
+  padding:20px 10px 20px 5px;
+}
+.username, .password {
+  margin-bottom: 30px;
+  border-radius: 25px;
+  box-shadow: inset 8px 8px 8px #cbced1,
+              inset -8px -8px 8px #ffffff;
+}
+.fields svg {
+  height: 22px;
+  margin:0 10px -3px 25px;
+}
+.signin-button {
+  outline: none;
+  border:none;
+  cursor: pointer;
+  width:100%;
+  height: 60px;
+  border-radius: 30px;
+  font-size: 20px;
+  font-weight: 700;
+  font-family: 'Lato', sans-serif;
+  color:black;
+  text-align: center;
+  background: #ecf0f3;
+  box-shadow: 3px 3px 8px #b1b1b1,
+              -3px -3px 8px #ffffff;
+  transition: 0.5s;
+}
+.signin-button:hover {
+  background:#ecf0f3;
+}
+.signin-button:active {
+  background:#ecf0f3;
+}
+.link {
+  padding-top: 20px;
+  text-align: center;
+}
+.link a {
+  text-decoration: none;
+  color:#aaa;
+  font-size: 15px;
+}
+</style>
+
+
+</head>
+
+<body>
+
+
+
+<div class="login-div">
+  <div class="logo"></div>
+  <div class="title">Red Stapler</div>
+  <div class="sub-title">BETA</div>
+  <div class="fields">
+    <div class="username"><input type="username" class="user-input" placeholder="username" /></div>
+    <div class="password"><input type="password" class="pass-input" placeholder="password" /></div>
+  </div>
+  <button class="signin-button">Login</button>
+  <div class="link">
+    <a href="#">Forgot password?</a> or <a href="#">Sign up</a>
+  </div>
+</div>
+
+
+
+    
+
+
+
+    <script>
+        
+ 
+
+
+  
+    </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+</body>
+
+</html>
+</script>
+</body>
+</html>
